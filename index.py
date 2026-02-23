d376472944f469b671695baf3e814bac622cd682
Dev-Ruqoyah <hamzatrukayatabiodun02@gmail.com>
2026-02-23 21:50:44 +0100

    type casting

---
+++ index.py
@@ -0,0 +1,54 @@
+
+#this is printing number 90
+# print(name)
+
+# number = 90
+# print(number)
+
+#DATA TYPES
+
+# name = emmanuel
+# print(name)
+
+
+#DATA TYPES --- 
+# integer (whole number), 
+# floating number --(decimal number), 
+# string -- ('emmanu899898')
+# boolean --- True or False
+# list
+# tuple
+# dictionary
+# set
+
+# ctrl /
+
+
+# INTEGER
+# number = 90
+# print(type(number))
+
+#FLOAT NUMBER
+# decimal = 45.665
+# print(decimal)
+# print(type(decimal))
+
+# print(56+34.6) 
+
+# STRING
+# print("Emmanuel")
+# print('Emmanuel0001111')
+
+# name = 'Emmanuel'
+# print(name)
+
+# department = "Computer Science"
+# print(department)
+# print(type(department))
+
+a = "730"
+print(a + "emmanuel")
+
+print(30 + 80)
+
+
---
+++ type.py
@@ -0,0 +1,26 @@
+# int(age)
+# print(type(age))
+# print(age + 12)
+
+int(),str(),float(),bool() 
+
+# age = int(age) #converting to an integer using int
+# print(age + 12)
+# print(type(age))
+
+# CONCATENATION - Addition of two strings together
+age = "12"
+# print(age + 3)
+print(int(age) + 3)
+
+num = 89000
+print(float(num))
+
+bool = True
+print(int(bool))
+
+second_value = False
+print(int(second_value))
+print(str(second_value)) 
+print(type(str(second_value)))
+
