312ac00086edc483855a3a6ff53226fadb13ffe6
theHrDev <hamzatrukayat2822@gmail.com>
2026-02-26 07:31:10 +0100

    input command

--- if.py
+++ if.py
@@ -1,1 +1,24 @@
-print(10)
+# input("What is your name: ")
+
+# name = input("What is your name: ")
+
+# print(name)
+# print(type(name))
+
+# age = input("Enter your age: ")
+# print(age)
+# print(age  + 12)
+# print(int(age) + 12)
+# print(type(age))
+
+age = input("Enter your age: ")
+# print(age)
+print(f"You are {age} years old")
+
+
+
+# Assignment
+first_num
+second_num
+
+the sum of the two number
